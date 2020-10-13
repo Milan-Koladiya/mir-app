a55e10fe995ea04e3a62fec043aee7ca5e2f3b74
Milan Koladiya <milankoladiya444@gmail.com>
2020-10-13 22:33:11 +0530

    added some css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import Home from "./components/home";
+import Mypost from "./components/mypost";
+import Myprofile from "./components/myprofile";
+import Signup from "./components/signup";
+import Login from "./components/login";
+import ErrorPage from "./components/errorpage";
+import Addpost from "./components/addpost";
 
 function App() {
+  const history = useHistory();
+  // const [IsLogin, setIsLogin] = useState(false)
+
+  // useEffect(() => {
+  //   if (localStorage.getItem('jwt')) {
+  //     setIsLogin(true);
+  //   }
+  // }, [])
+
+  if (localStorage.getItem("Username")) {
+    history.push("/");
+  } else {
+    history.push("/login");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      {/* <Header IsLogin={IsLogin} /> */}
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/addpost" component={Addpost} />
+        <Route path="/mypost" component={Mypost} />
+        <Route path="/myprofile" component={Myprofile} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route path="*" component={ErrorPage} />
+      </Switch>
     </div>
   );
 }
